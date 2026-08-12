21c212c55c7b616e1f50badf3dae165fc3d661a2
rovezuka <rostislav.04@list.ru>
2026-08-12 17:39:31 +0300

    step2

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/spf13/pflag"
 	"log"
+	"strings"
 )
 
 const (
@@ -12,15 +13,53 @@ const (
 
 func main() {
 
-	userID := flag.Uint64("user", 0, "userID for add item to cart")
-	skuID := flag.Uint64("sku", 0, "skuID for add item to cart")
-	skuCount := flag.Uint64("count", 0, "count items")
+	userID := pflag.Uint64("user", 0, "userID for add item to cart")
+	skuID := pflag.Uint64("sku", 0, "skuID for add item to cart")
+	skuCount := pflag.Uint("count", 0, "count items")
 
-	flag.Parse()
+	pflag.Parse()
 
-	if flag.NFlag() != expectedFlagsCnt {
-		log.Fatalf("unexpected number of arguments: %d, want: %d", flag.NFlag(), expectedFlagsCnt)
+	if pflag.NFlag() != expectedFlagsCnt {
+		log.Fatalf("unexpected number of arguments: %d, want: %d", pflag.NFlag(), expectedFlagsCnt)
 	}
 
-	fmt.Printf("user: %d, sku: %d, sku_cnt: %d\n", *userID, *skuID, *skuCount)
+	Add(*userID, *skuID, *skuCount)
+
+	fmt.Println(Read(*userID))
+}
+
+func Read(userID uint64) string {
+	builder := strings.Builder{}
+	for _, ci := range store[userID] {
+		builder.Write([]byte(fmt.Sprintf("sku: %d - cnt: %d\n", ci.SKU, ci.QTY)))
+	}
+	return builder.String()
+}
+
+type CartItem struct {
+	SKU uint64
+	QTY uint
+}
+
+//type CartItem map[uint64]uint
+
+var store = make(map[uint64]map[uint64]*CartItem)
+
+func Add(userID, sku uint64, skuCount uint) {
+	if cartItems, ok := store[userID]; ok {
+		if cartItem, ok := cartItems[sku]; ok {
+			cartItem.QTY += skuCount
+		} else {
+			cartItem = &CartItem{
+				SKU: sku,
+				QTY: skuCount,
+			}
+		}
+	}
+	store[userID] = make(map[uint64]*CartItem)
+	store[userID][sku] = &CartItem{
+		SKU: sku,
+		QTY: skuCount,
+	}
+	//fmt.Printf("user: %d, sku: %d, sku_cnt: %d\n", userID, skuID, skuCount)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module CliCart
 
 go 1.26.2
+
+require github.com/spf13/pflag v1.0.10
